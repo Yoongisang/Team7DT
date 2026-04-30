f6c18630066cdbf1991928ecde9b69fa0fa02c3b
yoonseo <cysyes0403@naver.com>
2026-04-30 09:57:03 +0900

    feat: h/cpp 파일 추가

---
+++ Source/Team7DT/Public/Sensor/LidarBevRenderer.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "LidarBevRenderer.generated.h"
+
+
+UCLASS()
+class TEAM7DT_API ULidarBevRenderer : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/Team7DT/Public/Sensor/SensorViewWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SensorViewWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM7DT_API USensorViewWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
